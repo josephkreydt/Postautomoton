9c80a0017509dea1832a810cc4fbba35191224ba
Joseph <8309575+josephkreydt@users.noreply.github.com>
2021-01-24 14:41:10 -0500

    Added some functions

--- postautomoton.py
+++ postautomoton.py
@@ -1,30 +1,61 @@
+#
+# Check RSS feed for posts
+# Check Mastodon timeline for status updates about RSS feed posts
+# If no status update about post, post status update about it
+#
+
 import requests
 import feedparser
 
+# Set URL of RSS feed to check
 rssfeed = 'https://bitsrfr.com/rss/'
+
+# Set authorization token of Mastodon account
+auth_token = ''
+
+# Set hostname (e.g. mstdn.social) of Mastodon account
+mastodon_host = 'mstdn.social'
 
 def feedchecker(url):
     feed = feedparser.parse(url)
 
     entries = feed.entries
-    post_info_set = set()
+    post_info_dict= {}
     post_list = []
 
     for entry in entries:
-        post_info_set.clear()
-        post_info_set.add(entry.title)
-        #print(entry.title)
-        post_info_set.add(entry.published)
-        #print(entry.published)
-        post_list.append(post_info_set.copy())
-        #print(post_list)
-        #return entry.title
-        #return entry.published
+        post_info_dict.update({'title': entry.title, 'date': entry.published})
+        post_list.append(post_info_dict.copy())
     return post_list
 
-post_list = feedchecker(rssfeed)
+def getid(auth_token, mastodon_host):
+    api_url = f'https://{mastodon_host}/api/v1/accounts/verify_credentials'
+    auth_header = {'Authorization': f'Bearer {auth_token}'}
 
-print(post_list)
+    response = requests.get(api_url, headers=auth_header)
+
+    json_response = response.json()
+    account_id = json_response['id']
+    return account_id
+
+def statuschecker(mastodon_host, account_id):
+    api_url = f'https://{mastodon_host}/api/v1/accounts/{account_id}/statuses'
+
+    response = requests.get(api_url)
+
+    status_updates = response.json()
+
+    status_update_list = []
+
+    for entry in status_updates:
+        status_update_list.append(entry['content'])
+    return status_update_list
+
+post_list = feedchecker(rssfeed)
+account_id = getid(auth_token, mastodon_host)
+status_list = statuschecker(mastodon_host, account_id)
+
+
 
 '''
 url = 'https://mstdn.social/api/v1/statuses'
@@ -36,3 +67,5 @@ r = requests.post(url, data=params, headers=auth)
 
 print(r)
 '''
+
+# Joseph Kreydt
