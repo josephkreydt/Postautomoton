57f1d78474b69ca1c95afbe23afc6d8e02e70eb1
Joseph <8309575+josephkreydt@users.noreply.github.com>
2021-01-24 14:12:52 -0500

    Fixed issue with set/dict referencing instead of copying

--- postautomoton.py
+++ postautomoton.py
@@ -7,12 +7,17 @@ def feedchecker(url):
     feed = feedparser.parse(url)
 
     entries = feed.entries
-    post_info_dict = {}
+    post_info_set = set()
     post_list = []
 
     for entry in entries:
-        post_info_dict.update({'title': entry.title, 'date': entry.published})
-        post_list.append(post_info_dict)
+        post_info_set.clear()
+        post_info_set.add(entry.title)
+        #print(entry.title)
+        post_info_set.add(entry.published)
+        #print(entry.published)
+        post_list.append(post_info_set.copy())
+        #print(post_list)
         #return entry.title
         #return entry.published
     return post_list
